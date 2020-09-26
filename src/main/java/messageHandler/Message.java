fad0d7ebf6f5386efcdea6adc7f475d5b33ee531
Johnson Hao <breaking.lover@gmail.com>
2020-09-25 21:25:19 -0400

    added message utility class ===> to be updated

--- src/main/java/messageHandler/Message.java
+++ src/main/java/messageHandler/Message.java
@@ -1,2 +1,18 @@
-package messageHandler;public class Message {
+package messageHandler;
+
+import java.util.Date;
+import java.util.HashMap;
+
+public class Message {
+    private void postMessage(String user, String message){
+
+    }
+
+    private HashMap<Date, String> listMessages(Date ... date){
+        return new HashMap<>();
+    }
+
+    private void ClearChat(Date ... date){
+
+    }
 }
