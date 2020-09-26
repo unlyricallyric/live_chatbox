e563292bdedfd9984bd68172f63a02c5a6e84a74
Johnson Hao <breaking.lover@gmail.com>
2020-09-25 21:24:59 -0400

    added basic message servlet ===> to be updated

---
+++ src/main/java/MessageServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MessageServlet")
+public class MessageServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloWorldServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HelloWorldServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (IOException exception){
+            exception.getMessage();
+        }
+    }
+}
---
+++ src/main/java/messageHandler/Message.java
@@ -0,0 +1,2 @@
+package messageHandler;public class Message {
+}
