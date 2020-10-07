5be9ac91aa26d1b87dbf087e1f66311dbb463af4
Johnson Hao <breaking.lover@gmail.com>
2020-10-06 23:51:02 -0400

    added testing block for messageservlet

--- src/main/java/com/message/servlet/MessageServlet.java
+++ src/main/java/com/message/servlet/MessageServlet.java
@@ -1,5 +1,8 @@
 package com.message.servlet;
 
+import com.message.handler.ChatManager;
+import com.message.handler.Message;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalTime;
+import java.util.HashMap;
 
 @WebServlet("/MessageServlet")
 public class MessageServlet extends HttpServlet {
@@ -17,6 +22,18 @@ public class MessageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String message = request.getParameter("message");
         String user_name = request.getParameter("user_name");
+
+        //For testing outcome
+        /*ChatManager.postMessage(user_name, message);
+
+        HashMap<String, String> new_db;
+
+        ChatManager.postTestMessage(user_name, message);
+
+        new_db = ChatManager.listTestMessage();
+
+        PrintWriter out = response.getWriter();
+        out.println(new_db.get(user_name));*/
 
         String url = "/index.jsp";
         request.setAttribute("message", message);
