e150990542b839026d9be8fed0c2bdbfa4ad3ffa
Johnson Hao <breaking.lover@gmail.com>
2020-10-08 00:49:38 -0400

    added date range functionality but to be continued

--- src/main/java/com/message/servlet/MessageServlet.java
+++ src/main/java/com/message/servlet/MessageServlet.java
@@ -2,6 +2,7 @@ package com.message.servlet;
 
 import com.message.handler.ChatManager;
 import com.message.handler.Message;
+import com.message.service.MessageService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,43 +11,61 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.HashMap;
+import java.time.format.DateTimeFormatter;
+import java.util.TreeMap;
 
 @WebServlet("/MessageServlet")
 public class MessageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String message = request.getParameter("message");
         String user_name = request.getParameter("user_name");
 
-        //For testing outcome
         ChatManager.postMessage(user_name, message);
 
-        HashMap<LocalDateTime, Message> msg_db;
-
-        ChatManager.postTestMessage(user_name, message);
+        /*====temporary for testing outcome====*/
+        TreeMap<LocalTime, Message> msg_db;
 
         msg_db = ChatManager.ListMessages();
 
-        /*PrintWriter out = response.getWriter();
-        out.println(msg_db);
+        PrintWriter out = response.getWriter();
 
-        for (LocalDateTime i : msg_db.keySet()){
+        for (LocalTime i : msg_db.keySet()){
             out.println(msg_db.get(i).getDate());
             out.println(msg_db.get(i).getMessage());
             out.println(msg_db.get(i).getUsername());
-        }*/
+        }
+        /*====temporary for testing outcome====*/
 
-        String url = "/index.jsp";
+        /*====keep for sending back message object to frontend====*/
+        /*String url = "/index.jsp";
         request.setAttribute("msg_db", msg_db);
 
         getServletContext()
                 .getRequestDispatcher(url)
-                .forward(request, response);
+                .forward(request, response);*/
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get from and to params
+        String str_from = request.getParameter("from");
+        String str_to = request.getParameter("to");
+
+        LocalTime from = MessageService.getDateFromString(str_from);
+        LocalTime to = MessageService.getDateFromString(str_to);
+
+        //TODO implement logic while only one of the range is given
+
+        TreeMap<LocalTime, Message> msg_db;
+
+        msg_db = ChatManager.ListMessages(from, to);
+
+        PrintWriter out = response.getWriter();
+
+        for (LocalTime i : msg_db.keySet()){
+            out.println(msg_db.get(i).getDate());
+            out.println(msg_db.get(i).getMessage());
+            out.println(msg_db.get(i).getUsername());
+        }
     }
 }
