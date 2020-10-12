0d681940d05e99be19d505d9189ccb14af16e39c
Johnson Hao <breaking.lover@gmail.com>
2020-10-12 19:57:05 -0400

    added logic to determine delete or get message

--- src/main/java/com/message/servlet/MessageServlet.java
+++ src/main/java/com/message/servlet/MessageServlet.java
@@ -14,19 +14,19 @@ import java.io.PrintWriter;
 import java.time.LocalTime;
 import java.util.TreeMap;
 
-@WebServlet("/MessageServlet")
+@WebServlet("/MessageServlet/*")
 public class MessageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /*
             Referer Check
          */
-        String refererHeader = request.getContextPath();
+        /*String refererHeader = request.getContextPath();
         System.out.println("getHeader "+request.getHeader("referer"));
         System.out.println("getContextPath "+request.getContextPath());
         System.out.println("getPathInfo "+request.getPathInfo());
         System.out.println("getHttpServletMapping "+request.getHttpServletMapping());
         System.out.println("getRequestURI "+request.getRequestURI());
-        System.out.println("getRequestURL "+request.getRequestURL());
+        System.out.println("getRequestURL "+request.getRequestURL());*/
 
         String message = request.getParameter("message");
         String user_name = request.getParameter("user_name");
@@ -52,9 +52,6 @@ public class MessageServlet extends HttpServlet {
             msg_arr[count++][2] = msg_db.get(i).getMessage();
         }
 
-        out.println(msg_arr.toString());
-        /*====temporary for testing outcome====*/
-
         /*====keep for sending back message object to frontend====*/
         String url = "/index.jsp";
         request.setAttribute("msg_db", msg_db);
@@ -66,10 +63,16 @@ public class MessageServlet extends HttpServlet {
                 .forward(request, response);
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //get from and to params
         String str_from = request.getParameter("from");
         String str_to = request.getParameter("to");
+        String user_name = request.getParameter("user_name");
+
+        System.out.println(request.getParameter("displayMessage"));
+        System.out.println(str_from);
+        System.out.println(str_to);
 
         LocalTime from = (MessageService.isNullOrEmpty(str_from))?LocalTime.of(00,00,00):MessageService.getDateFromString(str_from);
         LocalTime to = (MessageService.isNullOrEmpty(str_to))?LocalTime.of(23,59,59):MessageService.getDateFromString(str_to);
@@ -77,25 +80,38 @@ public class MessageServlet extends HttpServlet {
         //TODO implement logic while only one of the range is given
         TreeMap<LocalTime, Message> msg_db;
 
+        String msg_req = request.getParameter("displayMessage");
+        String url = "";
+
+        if(msg_req.equals("submit_delete")){
+            url = "/index.jsp";
+            ChatManager.clearChat(from, to);
+        }else{
+            url = "/show_message.jsp";
+        }
+
         if(MessageService.isNullOrEmpty(str_from) && MessageService.isNullOrEmpty(str_to)){
             msg_db = ChatManager.ListMessages();
         }else{
             msg_db = ChatManager.ListMessages(from, to);
         }
 
-        PrintWriter out = response.getWriter();
+        request.setAttribute("msg_db", msg_db);
+        request.setAttribute("user_name", user_name);
 
-        for (LocalTime i : msg_db.keySet()){
-            out.println(msg_db.get(i).getDate());
-            out.println(msg_db.get(i).getMessage());
-            out.println(msg_db.get(i).getUsername());
-        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+
     }
 
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //get from and to params
         String str_from = request.getParameter("from");
         String str_to = request.getParameter("to");
+
+        System.out.println("This is from : "+str_from);
+        System.out.println("This is to : "+str_to);
 
         LocalTime from = (MessageService.isNullOrEmpty(str_from))?LocalTime.of(00,00,00):MessageService.getDateFromString(str_from);
         LocalTime to = (MessageService.isNullOrEmpty(str_to))?LocalTime.of(23,59,59):MessageService.getDateFromString(str_to);
---
+++ src/main/webapp/show_message.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: johnsonhao
+  Date: 2020-10-12
+  Time: 4:10 p.m.
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
