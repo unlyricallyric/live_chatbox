fc5d5e88a985e43e938a06836d519f90f7504ac4
Johnson Hao <breaking.lover@gmail.com>
2020-10-15 00:46:09 -0400

    added cross site request forgery check

--- src/main/java/com/message/service/MessageService.java
+++ src/main/java/com/message/service/MessageService.java
@@ -2,6 +2,10 @@ package com.message.service;
 
 import com.message.handler.Message;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.TreeMap;
@@ -15,12 +19,23 @@ public class MessageService {
     }
 
     public static TreeMap<LocalTime, Message> getMsgWithRange(TreeMap DB, LocalTime from, LocalTime to){
-
         return DB;
     }
 
     //check if msg is empty or null
     public static boolean isNullOrEmpty(String str){
         return (str == null || str.trim().isEmpty());
+    }
+
+    public static void csrfValidate(String referer, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //return referer.startsWith("http://localhost:8080");
+        if(!referer.startsWith("http://localhost:8080")) {
+            request.getServletContext()
+                    .getRequestDispatcher("/error_handling/error-404.jsp")
+                    .forward(request, response);
+        }
+    }
+
+    public static void main(String[] arg){
     }
 }
