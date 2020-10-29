1e9ebfea3bde067ba890f13785b72f4902d23315
Johnson Hao <breaking.lover@gmail.com>
2020-10-29 01:10:08 -0400

    setup ssh tunnel and connection function

--- src/main/java/com/message/CTestDriver.java
+++ src/main/java/com/message/CTestDriver.java
@@ -1,11 +1,9 @@
 package com.message;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 import com.jcraft.jsch.JSch;
@@ -28,31 +26,46 @@ public class CTestDriver
         session.setPortForwardingL(nLocalPort, strRemoteHost, nRemotePort);
     }
 
-    public static void main(String[] args) throws IOException {
+    private static Connection getConnection() throws IOException, JSchException, ClassNotFoundException, SQLException {
         Properties prop = new Properties();
-        String propFile = "/config.properties";
+        String propFileName = "config.properties";
 
-        InputStream inputStream = CTestDriver.class.getClassLoader().getResourceAsStream(propFile);
-        prop.load(inputStream);
+        InputStream inputStream = CTestDriver.class.getClassLoader().getResourceAsStream(propFileName);
 
-        System.exit(1);
-        try
-        {
-            String strSshUser = "root";                  // SSH loging username
-            String strSshPassword = "O2q3C4al";                   // SSH login password
-            String strSshHost = "192.99.246.175";          // hostname or ip or SSH server
-            int nSshPort = 22;                                    // remote SSH host port number
-            String strRemoteHost = "localhost";  // hostname or ip of your database server
-            int nLocalPort = 3366;                                // local port number use to bind SSH tunnel
-            int nRemotePort = 3306;                               // remote port number of your database
-            String strDbUser = "root";                    // database loging username
-            String strDbPassword = "Concordia2020";                    // database login password
+        if (inputStream != null) {
+            prop.load(inputStream);
+        } else {
+            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+        }
 
-            CTestDriver.doSshTunnel(strSshUser, strSshPassword, strSshHost, nSshPort, strRemoteHost, nLocalPort, nRemotePort);
+        Date time = new Date(System.currentTimeMillis());
 
-            Class.forName(
-                    "com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:"+nLocalPort+"/testDB", strDbUser, strDbPassword);
+        // get the property value and print it out
+        String host_ip = prop.getProperty("host_ip");
+        String host_user = prop.getProperty("host_user");
+        String host_pass = prop.getProperty("host_pass");
+        int remote_port = Integer.parseInt(prop.getProperty("remote_port"));
+        int bind_port = Integer.parseInt(prop.getProperty("bind_port"));
+        String db_host = prop.getProperty("db_host");
+        int db_port = Integer.parseInt(prop.getProperty("db_port"));
+        String db = prop.getProperty("db");
+        String db_user = prop.getProperty("db_user");
+        String db_pass = prop.getProperty("db_pass");
+
+        CTestDriver.doSshTunnel(host_user, host_pass, host_ip, remote_port, db_host, bind_port, db_port);
+
+        Class.forName(
+                "com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:"+bind_port+"/"+db, db_user, db_pass);
+
+        return con;
+    }
+
+    public static void main(String[] args) throws IOException, JSchException, SQLException, ClassNotFoundException {
+
+        Connection con = getConnection();
+
+        try {
 
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM Persons");
@@ -61,16 +74,10 @@ public class CTestDriver
                 String lastName = rs.getString("LastName");
                 System.out.println(lastName + "\n");
             }
+        }catch(Exception e){
 
+        }finally {
             con.close();
-        }
-        catch( Exception e )
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            System.exit(0);
         }
     }
 }
