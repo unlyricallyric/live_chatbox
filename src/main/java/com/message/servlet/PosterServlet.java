1628abfea625d4b09bff4922446c17683231e9c7
Johnson Hao <breaking.lover@gmail.com>
2020-10-29 00:44:11 -0400

    initialize poster servlet

--- src/main/java/com/message/servlet/PosterServlet.java
+++ src/main/java/com/message/servlet/PosterServlet.java
@@ -1,2 +1,15 @@
-package com.message.servlet;public class PosterServlet {
+package com.message.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/PosterServlet/*")
+public class PosterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
