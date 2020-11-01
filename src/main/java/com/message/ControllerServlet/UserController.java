3d26e9c45608db8acc61bd54ddd7e5a43a5cb0de
Johnson Hao <breaking.lover@gmail.com>
2020-11-01 14:44:37 -0500

    created Controller class for user

--- src/main/java/com/message/ControllerServlet/UserController.java
+++ src/main/java/com/message/ControllerServlet/UserController.java
@@ -1,10 +1,19 @@
-@javax.servlet.annotation.WebServlet(name = "UserController")
-public class UserController extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package com.message.ControllerServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "UserController")
+public class UserController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 }
