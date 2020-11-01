fb7e6824879226b4121a60e048aa6cb87a977971
Johnson Hao <breaking.lover@gmail.com>
2020-11-01 14:44:02 -0500

    sql backup statement

--- src/main/resources/statement.sql
+++ src/main/resources/statement.sql
@@ -1,14 +1,24 @@
 /*Create database*/
 CREATE DATABASE IF NOT EXISTS Blog;
 
+
 CREATE TABLE Users (
     user_id int NOT NULL AUTO_INCREMENT,
-    user_name varchar(32) NOT NULL,
-    password varchar(255),
-    user_email varchar(32),
-    first_name varchar(32),
-    last_name varchar(32),
+    user_name VARCHAR(32) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_email VARCHAR(32),
+    first_name VARCHAR(32),
+    last_name VARCHAR(32),
     PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Posts (
+    id int NOT NULL AUTO_INCREMENT,
+    posted_by VARCHAR(32) NOT NULL,
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    message TEXT,
+    PRIMARY KEY (id)
 );
 
 /*Insert dummy data to Users table*/
