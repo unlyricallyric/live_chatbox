3fa96c55ede357712228ae5106497a7f437be744
Johnson Hao <breaking.lover@gmail.com>
2020-11-02 21:52:40 -0500

    updated basic CRUD

--- src/main/java/com/message/servlet/PosterServlet.java
+++ src/main/java/com/message/servlet/PosterServlet.java
@@ -6,10 +6,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
-@WebServlet("/PosterServlet/*")
+@WebServlet("/")
 public class PosterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getServletPath();
+
+        System.out.println("this is the action: " + action);
+
+        try{
+            switch (action) {
+                case "/PosterServlet/create":
+                    createPost(request, response);
+                    break;
+                case "/PosterServlet/read":
+                    listPost(request, response);
+                    break;
+                case "/PosterServlet/update":
+                    updatePost(request, response);
+                    break;
+                case "/PosterServlet/delete":
+                    deletePost(request, response);
+                    break;
+                default:
+                    listPost(request, response);
+                    break;
+            }
+        }catch(SQLException ex){
+            throw new ServletException(ex);
+        }
+    }
+
+    private void createPost(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException{
+        System.out.println("this is Create");
+    }
+
+    private void listPost(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException{
+        System.out.println("this is Read");
+    }
+
+    private void updatePost(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException{
+        System.out.println("this is update");
+    }
+
+    private void deletePost(HttpServletRequest request, HttpServletResponse response) throws IOException, SQLException{
+        System.out.println("this is delete");
     }
 }
