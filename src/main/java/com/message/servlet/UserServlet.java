e1fc9b50af47004981f517e6acf95ccdf5f84206
Johnson Hao <breaking.lover@gmail.com>
2020-11-05 01:35:11 -0500

    updated user and post servlet routing functionality

--- src/main/java/com/message/servlet/UserServlet.java
+++ src/main/java/com/message/servlet/UserServlet.java
@@ -1,10 +1,60 @@
-@javax.servlet.annotation.WebServlet(name = "UserServlet")
-public class UserServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package com.message.servlet;
 
+import com.message.controller.UserController;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/UserServlet/*")
+public class UserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        String action = request.getPathInfo();
 
+        try {
+            switch (action) {
+                case "/create":
+                    createUser(request, response);
+                    break;
+                case "/update":
+                    updateUser(request, response);
+                    break;
+                case "/delete":
+                    deleteUser(request, response);
+                    break;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void createUser(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("this is Create");
+
+        try{
+            UserController uc = new UserController();
+            //String user_name = uc.findUser(1);
+
+            //System.out.println("the user name is: " + user_name);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateUser(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("this is Create");
+    }
+
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("this is Create");
     }
 }
