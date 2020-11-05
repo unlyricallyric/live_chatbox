61449f934ac596915bb7d85b7a9ac3e0109dd875
Johnson Hao <breaking.lover@gmail.com>
2020-11-05 01:33:22 -0500

    added data access object for file user and post

--- src/main/java/com/message/dao/PostDao.java
+++ src/main/java/com/message/dao/PostDao.java
@@ -1,4 +1,11 @@
-package com.message.Dao;
+package com.message.dao;
+
+import com.message.model.Post;
 
 public interface PostDao {
+    boolean createPost(Post post);
+    Post getRecentPost();
+    Post getAllPost();
+    boolean updatePost(int post_id);
+    boolean deletePost(int post_id);
 }
