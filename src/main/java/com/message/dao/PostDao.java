4e20474470775be40ac19933b2106829c8c0756d
Johnson Hao <breaking.lover@gmail.com>
2020-11-05 23:56:51 -0500

    updated Post data access object

--- src/main/java/com/message/dao/PostDao.java
+++ src/main/java/com/message/dao/PostDao.java
@@ -3,9 +3,10 @@ package com.message.dao;
 import com.message.model.Post;
 
 public interface PostDao {
-    boolean createPost(Post post);
+    int createPost(Post post);
+    Post getSinglePost(int post_id);
     Post getRecentPost();
     Post getAllPost();
-    boolean updatePost(int post_id);
-    boolean deletePost(int post_id);
+    int updatePost(int post_id, String post_title, String message);
+    int deletePost(int post_id);
 }
