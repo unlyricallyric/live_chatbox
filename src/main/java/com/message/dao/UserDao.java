0434bae9d42519e529c3ece9754d71ea44db40a6
Johnson Hao <breaking.lover@gmail.com>
2020-11-07 00:04:56 -0500

    added create function

--- src/main/java/com/message/dao/UserDao.java
+++ src/main/java/com/message/dao/UserDao.java
@@ -1,8 +1,13 @@
 package com.message.dao;
 
+import com.message.model.User;
+
 import java.io.IOException;
 import java.sql.SQLException;
 
 public interface UserDao {
+
+    int createUser(User user);
     String findUser(int id) throws SQLException, IOException;
+
 }
--- src/main/java/com/message/servlet/UserServlet.java
+++ src/main/java/com/message/servlet/UserServlet.java
@@ -1,6 +1,8 @@
 package com.message.servlet;
 
 import com.message.controller.UserController;
+import com.message.model.User;
+import com.message.utils.BlogUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -38,12 +40,16 @@ public class UserServlet extends HttpServlet {
 
     private void createUser(HttpServletRequest request, HttpServletResponse response){
         System.out.println("this is Create");
+        String username = request.getParameter("username");
+        String password = BlogUtil.passEncoding(request.getParameter("password"));
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String email = request.getParameter("email");
 
+        User user = new User(username, password, firstname, lastname, email);
         try{
             UserController uc = new UserController();
-            //String user_name = uc.findUser(1);
-
-            //System.out.println("the user name is: " + user_name);
+            uc.createUser(user);
 
         } catch (Exception e) {
             e.printStackTrace();
