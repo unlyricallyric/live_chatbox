2672585aa598412f215e81d387a4b92aeb2468e8
Johnson Hao <breaking.lover@gmail.com>
2020-11-07 21:05:42 -0500

    update Json format

--- src/main/java/com/message/model/PostJson.java
+++ src/main/java/com/message/model/PostJson.java
@@ -3,15 +3,15 @@ package com.message.model;
 public class PostJson {
     Post post;
 
-    public PostJson(Post post_value){
-        this.post = post_value;
+    public PostJson(Post post){
+        this.post = post;
     }
 
     public Post getPost_value() {
         return post;
     }
 
-    public void setPost_value(Post post_value) {
-        this.post = post_value;
+    public void setPost_value(Post post) {
+        this.post = post;
     }
 }
