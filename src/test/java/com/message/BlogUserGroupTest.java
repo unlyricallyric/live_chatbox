2f73df4259e2b46bb17ca1a008d753955f2fd069
Johnson <breaking.lover@gmail.com>
2020-12-02 01:15:42 -0500

    added testcase to validate user group count

--- src/test/java/com/message/BlogUserGroupTest.java
+++ src/test/java/com/message/BlogUserGroupTest.java
@@ -1,6 +1,8 @@
 package com.message;
 
 import com.message.controller.UserController;
+import com.message.dao.UserManager;
+import com.message.impl.UserManagerFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +11,7 @@ import java.util.LinkedHashMap;
 
 public class BlogUserGroupTest {
 
+    //Verify if groups are valid from database
     @Test
     void validateUserGroupExist(){
 
@@ -37,5 +40,19 @@ public class BlogUserGroupTest {
 
         Assertions.assertTrue(testing_result, " There is an issue with user(id: " + issue_id + ") having group as: " + issue_group);
 
+    }
+
+    //validate counts of predefined Singleton user groups
+    @Test
+    void validateUserGroupCount() {
+        LinkedHashMap<Integer, String> groups = new LinkedHashMap<>();
+
+        UserManagerFactory uf = new UserManagerFactory();
+
+        UserManager obj = uf.getInstance("group1");
+
+        groups = obj.getGroups();
+
+        Assertions.assertTrue(groups.size() == 5, "There should only have 5 pre-defined groups");
     }
 }
